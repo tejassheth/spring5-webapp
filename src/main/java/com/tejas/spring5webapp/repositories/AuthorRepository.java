50c03858361a0a96ea1968a60940a0b9d5d05cf1
tejas.sheth <tejas.sheth@eclinicalworks.com>
2019-09-20 06:59:06 -0400

    create and added repositories BookRepository and AuthorRepository

---
+++ src/main/java/com/tejas/spring5webapp/repositories/AuthorRepository.java
@@ -0,0 +1,8 @@
+package com.tejas.spring5webapp.repositories;
+
+import com.tejas.spring5webapp.model.Author;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+}
