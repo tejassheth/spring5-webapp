8f672c0d1f3c877ac6a0c6e202c0aed6a222279e
tejas.sheth <tejas.sheth@eclinicalworks.com>
2019-09-23 05:40:03 -0400

    Assignment - Display List of Authors using AuthorController and thymeleaf authors.html

---
+++ src/main/java/com/tejas/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,21 @@
+package com.tejas.spring5webapp.controllers;
+
+import com.tejas.spring5webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthorController {
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @GetMapping("/authors")
+    public String getAuthors(Model model) {
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors";
+    }
+}
